28deb9a209d04d40c390fbcc210d9cfc2759afac
unknown <Mubs@.(none)>
2009-02-05 23:56:23 -0500

    fixing shortcut browselet

--- www/config/themes/default/content/listing_bottom.php
+++ www/config/themes/default/content/listing_bottom.php
@@ -3,3 +3,29 @@
   <?php echo $this->filebrowser->get_folder_property('text-bottom') ?>
 </div>
 <?php } ?>
+
+<?php
+$inline = $this->filebrowser->get_file_list("cut", null, true);
+if (sizeof($inline) > 0) { 
+  foreach($inline as $file) {
+    $url = $this->filebrowser->get_item_property($file->name, 'url');
+    if ($url == "") {
+      $url = $this->filebrowser->get_item_property($file->name, 'directory');
+    }
+    $name = $this->filebrowser->get_item_property($file->name, 'name');
+  ?>
+<div id='related' class='clearfix'>
+  <div style='padding-bottom: 5px;'>see also:<br/></div>
+  <div class='clearfix list'>
+    <div class='fileicon'>
+      <a href='<?php echo $url ?>'><img src='<?php echo view::get_view_url() ?>/images/i_dir_cut.gif' alt='' width='30' height='14' border='0' /></a>
+    </div>
+    <div class='filename'>
+      <a href='<?php echo $url ?>'><?php echo $name ?></a>
+    </div>
+    <div class='filecomment'></div>
+  </div>
+</div><!-- INLINE TEXT --->
+<?php 
+  }
+} ?>
--- www/config/themes/default/content/listing_top.php
+++ www/config/themes/default/content/listing_top.php
@@ -13,3 +13,13 @@ if (sizeof($inline) > 0) {
   <p><?php echo $this->filebrowser->get_folder_property('text-top'); ?></p>
 </div>
 <?php } ?>
+
+<?php 
+$inline = $this->filebrowser->get_file_list("img", "-b", true);
+if (sizeof($inline) > 0) { 
+  foreach($inline as $file) {
+  ?>
+  <img src='/directory/<?php echo $this->filebrowser->get_folder()."/".$file->name ?>' />
+<?php 
+  }
+} ?>
