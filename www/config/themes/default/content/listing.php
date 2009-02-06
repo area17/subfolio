8b1b4356c1b340afa9d4c4a6c1bec71b7b8e6e57
unknown <Mubs@.(none)>
2009-02-05 23:31:14 -0500

    Fix for Ticket #47, folder/icons should be clickable

--- www/config/themes/default/content/listing.php
+++ www/config/themes/default/content/listing.php
@@ -28,6 +28,17 @@
 $new_updated_start = strtotime("-7 days");
 foreach ($folders as $folder): 
     $kind = $this->filebrowser->get_kind($folder->name);
+
+    $url = "";
+	  switch ($kind) {
+			case "site" :
+			  $url = "/directory".$this->filebrowser->get_link($folder->name)."/index.html";
+        break;
+        
+			default:
+          $url = "".$this->filebrowser->get_link($folder->name);
+        break;  		
+    }	
 ?>
   <tr>
     <td>
@@ -56,26 +67,10 @@ foreach ($folders as $folder):
         $thumbnail = view::get_view_url()."/images/".$icon_file.$new.$updated.".gif";        
         
       ?>
-      <img src='<?php echo $thumbnail ?>' width='30' height='14' border='0' />
+      <a href="<?php echo $url ?>"><img src='<?php echo $thumbnail ?>' width='30' height='14' border='0' /></a>
     </td>
     <td class="filename">
-
-      <?php
-		  switch ($kind) {
-  			case "site" :
-  			  ?>
-  			  <a href="/directory<?php echo $this->filebrowser->get_link($folder->name); ?>/index.html"><?php echo $folder->name; ?></a>
-  			  <?php
-          break;
-          
-  			default:
-  			  ?>
-            <a href="<?php echo $this->filebrowser->get_link($folder->name); ?>"><?php echo $folder->name; ?></a>
-          <?php
-          break;  		
-      }	
-      ?>
-
+      <a href="<?php echo $url ?>"><?php echo $folder->name ?></a>
     </td>
     <td class="filesize">
     </td>
@@ -104,13 +99,9 @@ foreach ($folders as $folder):
           $updated = "_up";
       }
 
-  ?>
-  <tr>
-    <td>
-      <img src='<?php echo view::get_view_url() ?>/images/i_<?php echo $this->filebrowser->get_kind($file->name).$new.$updated; ?>.gif' width='30' height='14' border='0' />
-    </td>
-    <td class="filename">
-      <?php 
+      $target = "";
+      $url = "";
+
 		  switch ($kind) {
   			case "ai" :
   			case "eps" :
@@ -122,42 +113,39 @@ foreach ($folders as $folder):
   			case "ppt" :
   			case "doc" :
   			case "fnt" :
-  			case "suit" :
+  			case "suit":
   			case "rtf" :
   			case "zip" :
-  			  ?>
-  			  <a href="/directory<?php echo $this->filebrowser->get_link($file->name); ?>"><?php echo $file->name; ?></a>
-          <?php
+  			  $url = "/directory".$this->filebrowser->get_link($file->name);
   			  break;
 
   			case "pop" :
-            $width    = $this->filebrowser->get_item_property($file->name, 'width')    ? $this->filebrowser->get_item_property($file->name, 'width') : 800;
-            $height   = $this->filebrowser->get_item_property($file->name, 'height')   ? $this->filebrowser->get_item_property($file->name, 'height') : 600;
-            $url      = $this->filebrowser->get_item_property($file->name, 'url')      ? $this->filebrowser->get_item_property($file->name, 'url') : 'http://www.area17.com';
-            $name     = $this->filebrowser->get_item_property($file->name, 'name')     ? $this->filebrowser->get_item_property($file->name, 'name') : 'POPUP';
-            $style    = $this->filebrowser->get_item_property($file->name, 'style')    ? $this->filebrowser->get_item_property($file->name, 'style') : 'WINDOW';
-  			  ?>
-  			  <a href="javascript:pop('<?php echo $url ?>','<?php echo $name ?>',<?php echo $width ?>,<?php echo $height ?>,'<?php echo $style ?>');"><?php echo $file->name ?></a>
-  			  <?php
+          $width    = $this->filebrowser->get_item_property($file->name, 'width')    ? $this->filebrowser->get_item_property($file->name, 'width') : 800;
+          $height   = $this->filebrowser->get_item_property($file->name, 'height')   ? $this->filebrowser->get_item_property($file->name, 'height') : 600;
+          $url      = $this->filebrowser->get_item_property($file->name, 'url')      ? $this->filebrowser->get_item_property($file->name, 'url') : 'http://www.area17.com';
+          $name     = $this->filebrowser->get_item_property($file->name, 'name')     ? $this->filebrowser->get_item_property($file->name, 'name') : 'POPUP';
+          $style    = $this->filebrowser->get_item_property($file->name, 'style')    ? $this->filebrowser->get_item_property($file->name, 'style') : 'WINDOW';
+
+          $url = "javascript:pop('$url','$name',$width,$height,'$style');";
           break;
 
   			case "net" :
           $url = $this->filebrowser->get_item_property($file->name, 'url')    ? $this->filebrowser->get_item_property($file->name, 'url') : '';
           $target = $this->filebrowser->get_item_property($file->name, 'target')    ? $this->filebrowser->get_item_property($file->name, 'target') : '_blank';
-          
-          if ($url <> '') { ?>
-            <a target="<?php echo $target ?>" href="<?php echo $url ?>"><?php echo $file->name ?></a>
-          <? }
   			  break;
 
   			default:
-  			  ?>
-  			  <a href="<?php echo $this->filebrowser->get_link($file->name); ?>"><?php echo $file->name; ?></a>
-          <?php
+  			  $url = $this->filebrowser->get_link($file->name);
           break;  			
 	    }
-      ?>
-      
+
+  ?>
+  <tr>
+    <td>
+      <a <?php if ($target <> "") print "target='$target'" ?> href="<?php echo $url ?>"><img src='<?php echo view::get_view_url() ?>/images/i_<?php echo $this->filebrowser->get_kind($file->name).$new.$updated; ?>.gif' width='30' height='14' border='0' /></a>
+    </td>
+    <td class="filename">
+      <a <?php if ($target <> "") print "target='$target'" ?> href="<?php echo $url ?>"><?php echo $file->name ?></a>
     </td>
     <td class="filesize">
       <?php echo format::filesize($file->stats['size']); ?>
