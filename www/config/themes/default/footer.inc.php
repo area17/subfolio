e1fa90153e3a8449acf79671ffb6403f5fc85043
unknown <Mubs@.(none)>
2009-02-05 22:59:00 -0500

    fixed footer link and copyright year

--- www/config/themes/default/footer.inc.php
+++ www/config/themes/default/footer.inc.php
@@ -1,5 +1,5 @@
 <div id="footer" class="clearfix">
 
-	<p class="copyright">Subfolio Portable &copy; 2009 <a target="_blank" href="http://www.area17.com">Area 17</a> Media LLC &ndash; All rights reserved</p>
+	<p class="copyright">Subfolio Portable &copy; <?php echo date("Y"); ?> <a target="_blank" href="http://www.area17.com">Area 17</a> Media LLC &ndash; All rights reserved</p>
 	
 </div>
