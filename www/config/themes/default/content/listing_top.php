c58e1dd81c8722989edfe825538a7c895c7187f8
Mubashar Iqbal <Mubs@.(none)>
2009-02-09 22:44:19 -0500

    Fix for Ticket #61

--- www/config/themes/default/content/listing_top.php
+++ www/config/themes/default/content/listing_top.php
@@ -1,13 +1,3 @@
-<?php 
-$inline = $this->filebrowser->get_file_list("img", "-l", true);
-if (sizeof($inline) > 0) { 
-  foreach($inline as $file) {
-  ?>
-  <img src='/directory/<?php echo $this->filebrowser->get_folder()."/".$file->name ?>' />
-<?php 
-  }
-} ?>
-
 <?php if ($this->filebrowser->get_folder_property('text-top') <> '') { ?>
 <div class="listing-top">
   <p><?php echo $this->filebrowser->get_folder_property('text-top'); ?></p>
@@ -16,6 +6,16 @@ if (sizeof($inline) > 0) {
 
 <?php 
 $inline = $this->filebrowser->get_file_list("img", "-b", true);
+if (sizeof($inline) > 0) { 
+  foreach($inline as $file) {
+  ?>
+  <img src='/directory/<?php echo $this->filebrowser->get_folder()."/".$file->name ?>' />
+<?php 
+  }
+} ?>
+
+<?php 
+$inline = $this->filebrowser->get_file_list("img", "-l", true);
 if (sizeof($inline) > 0) { 
   foreach($inline as $file) {
   ?>
