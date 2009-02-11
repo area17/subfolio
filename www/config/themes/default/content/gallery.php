a10742bdfb96d85706d67a30eae4630c21a7909e
Mubashar Iqbal <Mubs@.(none)>
2009-02-11 10:04:36 -0500

    small images now clickable

--- www/config/themes/default/content/gallery.php
+++ www/config/themes/default/content/gallery.php
@@ -3,17 +3,11 @@
 ?>
 <div id="gallery">
   <ul class="gallery">
-  <?php foreach ($files as $file) { 
-    if ($file->needs_thumbnail()) { ?>
+  <?php foreach ($files as $file) { ?>
     <li>
       <a href="<?php echo $this->filebrowser->get_link($file->name); ?>"><img src="<?php echo $file->get_thumbnail_url() ?>" /></a>
       <p><?php echo $file->name ?></p>
     </li>
-    <? } else { ?>
-    <li>
-      <img src="<?php echo $file->get_url() ?>" />
-    </li>
-    <?php } ?>
   <?php } ?>
   </ul>
   <div class="clearfix"></div>
