2b795e1a43edf3e62ba840e1e913cd5e0199f17b
Mubashar Iqbal <Mubs@.(none)>
2009-02-11 10:12:26 -0500

    fix for small images link

--- www/config/themes/default/content/gallery.php
+++ www/config/themes/default/content/gallery.php
@@ -3,11 +3,17 @@
 ?>
 <div id="gallery">
   <ul class="gallery">
-  <?php foreach ($files as $file) { ?>
+  <?php foreach ($files as $file) { 
+    if ($file->needs_thumbnail()) { ?>
     <li>
       <a href="<?php echo $this->filebrowser->get_link($file->name); ?>"><img src="<?php echo $file->get_thumbnail_url() ?>" /></a>
       <p><?php echo $file->name ?></p>
     </li>
+    <? } else { ?>
+    <li>
+      <a href="<?php echo $this->filebrowser->get_link($file->name); ?>"><img src="<?php echo $file->get_url() ?>" /></a>
+    </li>
+    <?php } ?>
   <?php } ?>
   </ul>
   <div class="clearfix"></div>
