de1aafa906cecbcdaf54e2b6867b0582d5396caf
Mubashar Iqbal <Mubs@.(none)>
2009-02-23 15:22:09 -0500

    undo last commit

--- www/config/themes/default/content/listing_top.php
+++ www/config/themes/default/content/listing_top.php
@@ -20,22 +20,6 @@ if (sizeof($inline) > 0) {
 <?php } ?>
 
 <?php 
-$inline = $this->filebrowser->get_file_list("txt", "-t-intro", true);
-if (sizeof($inline) > 0) { 
-  $this->filebrowser->set_displayed_content(true);
-  foreach($inline as $file) {
-    ?>
-    <div id="listing-top" class="clearfix">
-    <?php
-      readfile($file->name);
-    ?>
-    </div>
-    <?php
-  }
-} ?>
-
-
-<?php 
 $inline = $this->filebrowser->get_file_list("img", "-b", true);
 if (sizeof($inline) > 0) { 
   $this->filebrowser->set_displayed_content(true);
