b2c9f44d3209b3ee970e993a9e981900b11bf99c
Mubashar Iqbal <Mubs@.(none)>
2009-02-23 15:16:26 -0500

    fix for top intro text

--- www/config/themes/default/content/listing_top.php
+++ www/config/themes/default/content/listing_top.php
@@ -20,6 +20,22 @@ if (sizeof($inline) > 0) {
 <?php } ?>
 
 <?php 
+$inline = $this->filebrowser->get_file_list("txt", "-t-intro", true);
+if (sizeof($inline) > 0) { 
+  $this->filebrowser->set_displayed_content(true);
+  foreach($inline as $file) {
+    ?>
+    <div id="listing-top" class="clearfix">
+    <?php
+      readfile($file->name);
+    ?>
+    </div>
+    <?php
+  }
+} ?>
+
+
+<?php 
 $inline = $this->filebrowser->get_file_list("img", "-b", true);
 if (sizeof($inline) > 0) { 
   $this->filebrowser->set_displayed_content(true);
