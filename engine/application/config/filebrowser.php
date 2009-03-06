0bc00f22b087c4adb4b383115c63df1e5a15ac36
Mubashar Iqbal <Mubs@.(none)>
2009-03-06 16:26:03 -0500

    cleaned up comments

--- engine/application/config/filebrowser.php
+++ engine/application/config/filebrowser.php
@@ -23,23 +23,30 @@ $config['auth_salt'] = 'some_random_string';
 $config['users_yaml_file']  = $users_folder."users.yml";
 $config['groups_yaml_file'] = $users_folder."groups.yml";
 
+$config['filekinds_yaml_file']  = $settings_folder."filekinds.yml";
+$config['language_yaml_file']   = $settings_folder."language.yml";
+
 //Load settings from yaml file
 $settings_file  = $settings_folder."settings.yml";
 $settings = Spyc::YAMLLoad($settings_file);
 
 //rint_r($settings);
 
-$config['site_name'] = isset($settings['site_name']) ? $settings['site_name'] : $config['site_name'];
-$config['site_domain'] = isset($settings['site_domain']) ? $settings['site_domain'] : $config['site_domain'];
+$config['site_name']           = isset($settings['site_name'])           ? $settings['site_name'] : $config['site_name'];
+$config['site_domain']         = isset($settings['site_domain'])         ? $settings['site_domain'] : $config['site_domain'];
 
-$config['theme'] = isset($settings['theme']) ? $settings['theme'] : $config['theme'];
-$config['directory'] = isset($settings['directory']) ? $site_folder.$settings['directory'] : $config['directory'];
+$config['theme']               = isset($settings['theme'])               ? $settings['theme'] : $config['theme'];
 
-$config['access_file'] = isset($settings['access_file']) ? $settings['access_file'] : $config['access_file'];
-$config['properties_file'] = isset($settings['properties_file']) ? $settings['properties_file'] : $config['properties_file'];
+$config['directory']           = isset($settings['directory'])           ? $site_folder.$settings['directory'] : $config['directory'];
 
-$config['users_yaml_file'] = isset($settings['users_yaml_file']) ? $users_folder.$settings['users_yaml_file'] : $config['users_yaml_file'];
-$config['groups_yaml_file'] = isset($settings['groups_yaml_file']) ? $users_folder.$settings['groups_yaml_file'] : $config['groups_yaml_file'];
+$config['access_file']         = isset($settings['access_file'])         ? $settings['access_file'] : $config['access_file'];
+$config['properties_file']     = isset($settings['properties_file'])     ? $settings['properties_file'] : $config['properties_file'];
 
-$config['thumbnail_width'] = isset($settings['thumbnail_width']) ? $config_folder.$settings['thumbnail_width'] : $config['thumbnail_width'];
-$config['thumbnail_height'] = isset($settings['thumbnail_height']) ? $config_folder.$settings['thumbnail_height'] : $config['thumbnail_height'];
+$config['users_yaml_file']     = isset($settings['users_yaml_file'])     ? $users_folder.$settings['users_yaml_file'] : $config['users_yaml_file'];
+$config['groups_yaml_file']    = isset($settings['groups_yaml_file'])    ? $users_folder.$settings['groups_yaml_file'] : $config['groups_yaml_file'];
+
+$config['thumbnail_width']     = isset($settings['thumbnail_width'])     ? $config_folder.$settings['thumbnail_width'] : $config['thumbnail_width'];
+$config['thumbnail_height']    = isset($settings['thumbnail_height'])    ? $config_folder.$settings['thumbnail_height'] : $config['thumbnail_height'];
+
+$config['filekinds_yaml_file'] = isset($settings['filekinds_yaml_file']) ? $settings_folder.$settings['filekinds_yaml_file'] : $config['filekinds_yaml_file'];
+$config['language_yaml_file']  = isset($settings['language_yaml_file'])  ? $settings_folder.$settings['language_yaml_file']  : $config['language_yaml_file'];
