72369db1c9dc54143b3111a8e362c4397a750164
Mubashar Iqbal <Mubs@.(none)>
2009-03-16 15:10:59 -0400

    make link optional, use folder as target if present, Ticket #78

--- config/themes/default/pages/elements/features.php
+++ config/themes/default/pages/elements/features.php
@@ -33,13 +33,19 @@ if (sizeof($file_features) > 0 || sizeof($prop_features) > 0) { ?>
   if (sizeof($file_features) > 0) {
     foreach ($file_features as $file_feature) { 
     $feature = Spyc::YAMLLoad($file_feature->name);
+    $feature_link = "";
+    if (isset($feature['link'])) {
+      $feature_link = $feature['link'];
+    } else if (isset($feature['folder'])) {
+      $feature_link = "/".$this->filebrowser->get_folder()."/".$feature['folder'];
+    }
     ?>
     <li>
       <div class="image">
-        <a href="<?php echo $feature['link'] ?>"><img src="/directory/<?php echo $this->filebrowser->get_folder() ?>/<?php echo $feature['image'] ?>"></a>
+        <a href="<?php echo $feature_link ?>"><img src="/directory/<?php echo $this->filebrowser->get_folder() ?>/<?php echo $feature['image'] ?>"></a>
       </div>
       <div class="info">
-        <a href="<?php echo $feature['link'] ?>"><h2><?php echo $feature['title'] ?></h2></a>
+        <a href="<?php echo $feature_link ?>"><h2><?php echo $feature['title'] ?></h2></a>
         <p><?php echo $feature['description'] ?></p>
       </div>
       <div class="clear"></div>
@@ -48,13 +54,20 @@ if (sizeof($file_features) > 0 || sizeof($prop_features) > 0) { ?>
   } ?>
 
   <?php if (sizeof($prop_features) > 0) {
-  foreach ($prop_features as $feature) { ?>
+  foreach ($prop_features as $feature) { 
+    $feature_link = "";
+    if (isset($feature['link'])) {
+      $feature_link = $feature['link'];
+    } else if (isset($feature['folder'])) {
+      $feature_link = "/".$this->filebrowser->get_folder()."/".$feature['folder'];
+    }
+  ?>
     <li>
       <div class="image">
-        <a href="<?php echo $feature['link'] ?>"><img src="/directory/<?php echo $this->filebrowser->get_folder() ?>/<?php echo $feature['image'] ?>"></a>
+        <a href="<?php echo $feature_link ?>"><img src="/directory/<?php echo $this->filebrowser->get_folder() ?>/<?php echo $feature['image'] ?>"></a>
       </div>
       <div class="info">
-        <a href="<?php echo $feature['link'] ?>"><h2><?php echo $feature['title'] ?></h2></a>
+        <a href="<?php echo $feature_link ?>"><h2><?php echo $feature['title'] ?></h2></a>
         <p><?php echo $feature['description'] ?></p>
       </div>
       <div class="clear"></div>
