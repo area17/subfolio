655a338d42f2112b3fb4a68b2bdbc7abd7463e36
Arnaud Mercier <arnaud@area17.com>
2009-05-29 18:38:36 +0200

    Using the same icon than the login page

--- config/themes/default/pages/notfound.php
+++ config/themes/default/pages/notfound.php
@@ -1,4 +1,4 @@
-<div style="padding-bottom:5px;"><img src="<?php echo view::get_view_url() ?>/images/system/i_404.gif" width="40" height="35" border="0" /></div>
+<img src='<?php echo view::get_view_url() ?>/images/system/authentification_exclam.gif' width='59' height='59' border='0' />
 <p><?php echo Kohana::lang('filebrowser.notfound');?><br/>
 <?php echo Kohana::lang('filebrowser.check_url_go_back', '<a href="'.dirname($this->filebrowser->file).'">Parent Directory</a>');?>
 </p>
