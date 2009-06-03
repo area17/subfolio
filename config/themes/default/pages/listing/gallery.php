dc0161c7e647a69a90b301839fd7f8edae8694e9
Mubashar Iqbal <Mubs@.(none)>
2009-06-02 23:30:32 -0400

    ticket #122, fixed theme for gallery display of images too large for thumbnail

--- config/themes/default/pages/listing/gallery.php
+++ config/themes/default/pages/listing/gallery.php
@@ -7,34 +7,41 @@ if (sizeof($files) > 0) {
 <div id="gallery" >
   <ul>
   <?php foreach ($files as $file) { 
-		if ($file->needs_thumbnail()) { $image_source = $file->get_thumbnail_url(); } else { $image_source = $file->get_url(); }
-			if ($file->has_custom_thumbnail()) { 
-			// Custom thumbnails -----------------------------------------------------------------------------
-			?>
-      <li>
-        <a href="<?php echo $this->filebrowser->get_link($file->name); ?>">
-					<div class="gallery_thumbnail">
-						<img src="<?php echo $image_source ?>" />
-					</div>
-        	<p><?php echo $file->name ?></p>
-				</a>
-      </li>
-    	<?php } else { 
-			// Genrerated or not thumbnails -----------------------------------------------------------------------------
-			?>
-	    <li>
-        <a href="<?php echo $this->filebrowser->get_link($file->name); ?>">
-					<div class="gallery_thumbnail generated">
-						<div class='hcenterer' style="width:<?php if (!$file->needs_thumbnail()) { echo $file->get_width(); } ?>px;">
-							<div class="vcenterer">
-								<img src="<?php echo $image_source ?>" />
-							</div>
-						</div>
-					</div>
-        	<p><?php echo $file->name ?></p>
-				</a>
-      </li>
-			<?php	}
+  		if ($file->needs_thumbnail()) { 
+  		  $image_source = $file->get_thumbnail_url(); 
+  		} else { 
+    		$image_source = $file->get_url(); 
+    	}
+
+      if ($image_source <> '') {
+  			if ($file->has_custom_thumbnail()) { 
+  			// Custom thumbnails -----------------------------------------------------------------------------
+  			?>
+        <li>
+          <a href="<?php echo $this->filebrowser->get_link($file->name); ?>">
+  					<div class="gallery_thumbnail">
+  						<img src="<?php echo $image_source ?>" />
+  					</div>
+          	<p><?php echo $file->name ?></p>
+  				</a>
+        </li>
+      	<?php } else { 
+  			// Genrerated or not thumbnails -----------------------------------------------------------------------------
+  			?>
+  	    <li>
+          <a href="<?php echo $this->filebrowser->get_link($file->name); ?>">
+  					<div class="gallery_thumbnail generated">
+  						<div class='hcenterer' style="width:<?php if (!$file->needs_thumbnail()) { echo $file->get_width(); } ?>px;">
+  							<div class="vcenterer">
+  								<img src="<?php echo $image_source ?>" />
+  							</div>
+  						</div>
+  					</div>
+          	<p><?php echo $file->name ?></p>
+  				</a>
+        </li>
+  			<?php	}
+			}
 		} ?>
   </ul>
 </div>
