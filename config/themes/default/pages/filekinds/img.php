db785c62dc7b84a38f25f739b94f03de9ef34692
Arnaud Mercier <arnaud@area17.com>
2009-06-12 17:49:29 +0200

    Added the info hide/switch mechanisme at the bottom of the image permalink page

--- config/themes/default/pages/filekinds/img.php
+++ config/themes/default/pages/filekinds/img.php
@@ -1,1 +1,58 @@
+<?php
+	// This is not very DRY (also used in files_and_foder) we have to refactor it...
+	// Also, we should remove any logic from views...
+	$new_updated_start = $this->filebrowser->get_updated_since_time();
+	$file_kind = $this->filekind->get_kind_by_file($file->name);
+	$kind = isset($file_kind['kind']) ? $file_kind['kind'] : '';
+	$kind_display = isset($file_kind['display']) ? $file_kind['display'] : '—';
+	$icon_file = "";
+	$comment  = $this->filebrowser->get_item_property($this->filebrowser->file, 'comment') ? $this->filebrowser->get_item_property($this->filebrowser->file, 'comment') : '';
+  
+	$filesize = format::filesize($file->stats['size']);
+	$filesize = format::filesize($file->stats['size']) ? format::filesize($file->stats['size']) : "—";
+	$new = false;
+  $updated = false;
+  if (false && $file->stats['ctime'] > $new_updated_start) {
+      $new = true;
+  } else if ($file->stats['mtime'] > $new_updated_start) {
+      $updated = true;
+  }
+  $icon_file = $this->filekind->get_icon_by_file($file_kind, $new, $updated);
+  $icon = view::get_view_url()."/images/icons/big/".$icon_file.".png";
+?>
+
 <img src='<?php echo $this->filebrowser->get_file_url(); ?>' />
+
+<div id="info">
+	
+	<a href="javascript:InfoHideSwitch('info-box','info-button');" id="info-button" class="">Info</a>
+	
+	<div id="info-box" class="hide">
+
+		<div id="download_box">
+
+			<a id="clickable-zone" href="<?php echo $this->filebrowser->get_file_url(); ?>">
+				<!-- Filename / comment -->
+				<p id="filename"><?php echo $this->filebrowser->file ?></p>
+				<p><?php echo $comment ?></p>
+			</a>
+
+			<!-- Infos -->
+			<dl>
+				<dt>Kind: </dt><dd><?php echo $kind_display ?></dd>
+				<dt>Last modified: </dt><dd><?php echo format::filedate($file->stats['mtime']) ?></dd>
+				<dt>Size: </dt><dd><?php echo $filesize ?></dd>
+			</dl>
+			<!-- Instructions -->
+		  <?php if ($file_kind && isset($file_kind['instructions'])) { ?>
+			<p id='instructions'>Instructions: <?php echo $file_kind['instructions'] ?></p>
+			<?php } ?>
+			<!-- Download -->
+			<a href="<?php echo $this->filebrowser->get_file_url(); ?>" id="download">Download</a>
+
+		</div>
+		
+		
+	</div>
+	
+</div>
