3abb05d6bca2f911562db84cdc15de54762317df
Mubashar Iqbal <Mubs@.(none)>
2009-06-15 23:57:49 -0400

    more error checking in download box

--- config/themes/default/pages/filekinds/_download_box.php
+++ config/themes/default/pages/filekinds/_download_box.php
@@ -2,18 +2,23 @@
 		
 	<a id="clickable-zone" href="<?php echo $this->filebrowser->get_file_url(); ?>">
 		<!-- Icon -->
-		<?php	if ($updated) { ?>
+		<?php	if (isset($updated) && $updated) { ?>
 			<span class="updated"><!-- --></span>
 		<?php	} ?>
-		<?php	if ($new) { ?>
+		<?php	if (isset($new) && $new) { ?>
 			<span class="new"><!-- --></span>
 		<?php	} ?>
+		<?php	if (isset($icon) && $icon <> '') { ?>
 		<img src='<?php echo $icon; ?>' />
+		<?php } ?>
 		<!-- Filename / comment -->
 		<p id="filename"><?php echo $this->filebrowser->file ?></p>
+		<?php	if (isset($comment) && $comment <> '') { ?>
 		<p><?php echo $comment ?></p>
+		<?php } ?>
 	</a>
-	
+
+	<?php	if (isset($kind)) { ?>
 	<!-- Infos -->
 	<dl>
 		<dt>Kind: </dt><dd><?php echo $kind_display ?></dd>
@@ -25,6 +30,7 @@
 	<p id='instructions'>Instructions: <?php echo format::get_rendered_text($file_kind['instructions']) ?></p>
 	<?php } ?>
 	<!-- Download -->
+	<?php } ?>
 	<a href="<?php echo $this->filebrowser->get_file_url(); ?>" id="download">Download</a>
 	
 </div>
