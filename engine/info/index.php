075b92cb28937e5c5ddc8377a8f9fac76f1421c6
Arnaud Mercier <arnaud@area17.com>
2009-06-17 17:24:53 +0200

    Styled the info page

--- engine/info/index.php
+++ engine/info/index.php
@@ -5,162 +5,222 @@
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 		<title>Subfolio Info</title>	
-	
-		<style type="text/css" media="screen">
-		table { width: 770px;}
-		</style>
+
+		<link href="css/info.css" type="text/css" rel="stylesheet" >
+		
 	</head>
 	<body>
-		<h1>Subfolio System Information</h1>
+		<h1 id="logo">Subfolio System Information</h1>
 
+		<hr/ >
+		
 		<?php
 			$version = file_get_contents('../VERSION.txt');
 		?>
-		<h2>Subfolio Version: <?php echo $version ?></h2>
-
-
-		<div id="tests">
-      <h3>Server Requirements</h3>
-			<table cellspacing="0">
+		<table cellspacing="0">
 			<tr>
-				<th>Apache Version</th>
+				<th class="label"><h2>Subfolio Version:</h2></th>
+				<td class="sign"></td>
+				<td class="version"><h2><?php echo $version ?></h2></td>
+				<td class="instructions"></td>
+			</tr>
+		</table>
+		
+		<hr/ >
+		
+		<table cellspacing="0">
+			<tr>
+				<td class="label" colspan="4"><h2>Server Requirements:</h2></td>
+			</tr>
+			<tr>
+				<th class="label">Apache Version</th>
 				<?php if (version_compare(apacheversion(), '2.0', '>=')): ?>
-				<td class="pass">OK</td>
-				<td class="info"><?php echo apacheversion() ?></td>
+					<td class="sign pass">Pass</td>
+					<td class="version"><?php echo apacheversion() ?></td>
+					<td class="instructions"></td>
 				<?php else: $failed = TRUE ?>
-				<td class="fail">Subfolio requires Apache 2.0 or newer.</td>
-				<td class="info"><?php echo apacheversion() ?></td>
+					<td class="sign fail">Failed</td>
+					<td class="version"><?php echo apacheversion() ?></td>
+					<td class="instructions exclam">Subfolio requires Apache 2.0 or newer</td>
 				<?php endif ?>
-			</tr>
-			<tr>
-				<th>PHP Version</th>
-				<?php if (version_compare(PHP_VERSION, '5.2', '>=')): ?>
-				<td class="pass">OK</td>
-				<td class="info"><?php echo PHP_VERSION ?></td>
-				<?php else: $failed = TRUE ?>
-				<td class="fail">Subfolio requires PHP 5.2 or newer.</td>
-				<td class="info"><?php echo PHP_VERSION ?></td>
-				<?php endif ?>
-			</tr>
-
-			<tr>
-				<th>GD</th>
-				<?php if (extension_loaded('gd')): ?>
-				<td class="pass">OK</td>
-				<td class="info"><?php echo gdversion() ?></td>
-				<?php else: ?>
-				<td class="fail">GD is not enabled</td>
-				<?php endif ?>
-			</tr>
-
-			<tr>
-			<th>SPL Enabled</th>
-			<?php if (function_exists('spl_autoload_register')): ?>
-			<td class="pass">OK</td>
-			<?php else: $failed = TRUE ?>
-			<td class="fail"><a href="http://php.net/spl">SPL</a> is not enabled.</td>
-			<?php endif ?>
 			</tr>
 			
-
 			<tr>
-				<th>PCRE UTF-8</th>
-				<?php if ( !function_exists('preg_match')): $failed = TRUE ?>
-				<td class="fail"><a href="http://php.net/pcre">PCRE</a> support is missing.</td>
-				<?php elseif ( ! @preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
-				<td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with UTF-8 support.</td>
-				<?php elseif ( ! @preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
-				<td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with Unicode property support.</td>
-				<?php else: ?>
-				<td class="pass">OK</td>
+				<th class="label">PHP Version</th>
+				<?php if (version_compare(PHP_VERSION, '5.2', '>=')): ?>
+					<td class="sign pass">Pass</td>
+					<td class="version"><?php echo PHP_VERSION ?></td>
+					<td class="instructions"></td>
+				<?php else: $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version"><?php echo PHP_VERSION ?></td>
+					<td class="instructions exclam">Subfolio requires PHP 5.2 or newer</td>
+				<?php endif ?>
+			</tr>
+				
+			<tr>
+				<th class="label">GD Enabled</th>
+				<?php if (extension_loaded('gd')): ?>
+					<td class="sign pass">Pass</td>
+					<td class="version"><?php echo gdversion() ?></td>
+					<td class="instructions"></td>
+				<?php else: $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version red">GD is not enabled</td>
+					<td class="instructions exclam">Ask your system administrator to install the <a href="http://php.net/gd" target="_blank">GD Library</a> on your server</td>
+				<?php endif ?>
+			</tr>
+				
+			<tr>
+				<th class="label">SPL Enabled</th>
+				<?php if (function_exists('spl_autoload_register')): ?>
+					<td class="sign pass">Pass</td>
+					<td class="version"></td>
+					<td class="instructions"></td>
+				<?php else: $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version red">SPL is not enabled</td>
+					<td class="instructions exclam">Ask your system administrator to add <a href="http://php.net/spl" target="_blank">SPL</a> support to your server</td>
 				<?php endif ?>
 			</tr>
 
+			<tr>
+				<th class="label">PCRE UTF-8</th>
+				<?php if ( ! function_exists('preg_match')): $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version red">PCRE support is missing</td>
+					<td class="instructions exclam">Ask your system administrator to add <a href="http://php.net/pcre" target="_blank">PCRE</a> support to your server</td>
+				<?php elseif ( ! @preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version red">No UTF-8 support</td>
+					<td class="instructions exclam"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with UTF-8 suppor.</td>
+				<?php elseif ( ! @preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
+					<td class="sign fail">Failed</td>
+					<td class="version red">No Unicode support</td>
+					<td class="instructions exclam"><a href="http://php.net/pcre" target="_blank">PCRE</a> has not been compiled with Unicode property support</td>
+				<?php else: ?>
+					<td class="sign pass">Pass</td>
+					<td class="version"></td>
+					<td class="instructions"></td>
+				<?php endif ?>
+			</tr>
 
 			<tr>
-				<th>Mod Rewrite</th>
-				<?php 
-				$have_module = apache_is_module_loaded('mod_rewrite');
-				if ($have_module) { ?>
-  				<td class="pass">OK</td>
+				<th class="label">Mod Rewrite</th>
+				<?php $have_module = apache_is_module_loaded('mod_rewrite'); ?>
+				<?php if ($have_module) { ?>
+					<td class="sign pass">Pass</td>
+					<td class="version"></td>
+					<td class="instructions"></td>
 				<?php } else if ($have_module == null) { ?>
-  				<td class="unkown">Unknown</td>
+					<td class="sign gloups">Unknown</td>
+					<td class="version">Unknown</td>
+					<td class="instructions exclam">We could not determine if Mod Rewrite was enabled or not</td>
 				<?php } else { ?>
-  				<td class="fail">Mod Rewrite is not enabled</td>
+					<td class="sign fail">Failed</td>
+					<td class="version red">Not enabled</td>
+					<td class="instructions exclam">Ask your system administrator to enable Mod Rewrite for your site</td>					
 				<?php } ?>
 			</tr>
 
 			<tr>
-				<th>.htaccess file</th>
+				<th class="label">.htaccess file</th>
 				<?php if (file_exists('../../.htaccess')): ?>
-				<td class="pass">OK</td>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
 				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">.htaccess</td>
-				<td class="action">Put instructions here</td>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/.htaccess</span></td>
+					<td class="instructions exclam">Rename the <i>htaccess</i> file on the root to <i>.htaccess</i> (just add a dot to the beginning of the name)</td>
 				<?php endif ?>
 			</tr>
+										
+		</table>
+		
+		<hr/ >
 
-      <tr>
-      <td colspan=4>
-      <h3>Settings</h3>
-      </td>
-      </tr>
-
+		<table cellspacing="0">
 			<tr>
-				<th>Settings YML</th>
+				<td class="label" colspan="4"><h2>Settings:</h2></td>
+			</tr>
+			<tr>
+				<th class="label">Settings file</th>
 				<?php if (file_exists('../../config/settings/settings.yml')): ?>
-				<td class="pass">Found</td>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
 				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">/config/settings/settings.yml</td>
-				<td class="action">Put instructions here</td>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/config/settings/settings.yml</span></td>
+					<td class="instructions exclam">Duplicate the <i>settings.sample.yml</i> file and rename it to <i>settings.yml</i></td>
 				<?php endif ?>
 			</tr>
 
 			<tr>
-				<th>File Kinds YML</th>
+				<th class="label">File Kinds file</th>
 				<?php if (file_exists('../../config/settings/filekinds.yml')): ?>
-				<td class="pass">Found</td>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
 				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">/config/settings/filekinds.yml</td>
-				<td class="action">Put instructions here</td>
-			<?php endif ?>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/config/settings/filekinds.yml</span></td>
+					<td class="instructions exclam">Duplicate the <i>filekinds.sample.yml</i> file and rename it to <i>filekinds.yml</i></td>
+				<?php endif ?>
 			</tr>
-
+						
 			<tr>
-				<th>Language YML</th>
+				<th class="label">Language file</th>
 				<?php if (file_exists('../../config/settings/language.yml')): ?>
-				<td class="pass">Found</td>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
 				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">/config/settings/language.yml</td>
-				<td class="action">Put instructions here</td>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/config/settings/language.yml</span></td>
+					<td class="instructions exclam">Duplicate the <i>language.sample.yml</i> file and rename it to <i>filekinds.yml</i></td>
+				<?php endif ?>
+			</tr>
+									
+		</table>
+
+		<hr/ >
+
+		<table cellspacing="0">
+			<tr>
+				<td class="label" colspan="4"><h2>Users &amp; groups:</h2></td>
+			</tr>
+			<tr>
+				<th class="label">Users file</th>
+				<?php if (file_exists('../../config/users/users.yml')): ?>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
+				<?php else: ?>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/config/users/users.yml</span></td>
+					<td class="instructions exclam">Duplicate the <i>users.sample.yml</i> file and rename it to <i>users.yml</i></td>
 				<?php endif ?>
 			</tr>
 
 			<tr>
-				<th>Users YML</th>
-				<?php if (file_exists('../../config/users/users.yml')): ?>
-				<td class="pass">Found</td>
-				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">/config/users/users.yml</td>
-				<td class="action">Put instructions here</td>
-			<?php endif ?>
-			</tr>
-
-			<tr>
-				<th>Groups YML</th>
+				<th class="label">Groups file</th>
 				<?php if (file_exists('../../config/users/groups.yml')): ?>
-				<td class="pass">Found</td>
+					<td class="sign pass">Pass</td>
+					<td class="version">found</td>
+					<td class="instructions"></td>
 				<?php else: ?>
-				<td class="pass">Not found</td>
-				<td class="info">/config/users/groups.yml</td>
-				<td class="action">Put instructions here</td>
-			<?php endif ?>
+					<td class="sign fail">Failed</td>
+					<td class="version"><span class='red'>Not found</span><span class='grey'>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;/config/users/groups.yml</span></td>
+					<td class="instructions exclam">Duplicate the <i>groups.sample.yml</i> file and rename it to <i>groups.yml</i></td>
+				<?php endif ?>
 			</tr>
+												
+		</table>
+		
+
+
 
 			<?php /* ?>			
 			<tr>
@@ -216,10 +276,12 @@
 			<?php endif ?>
 			</tr>
 				<?php */ ?>
-			</table>
-		</div>
 
-		<p>If you wish to disable this page, create a blank file: /engine/info/disabled.php</p>
+				<hr/ >
+
+		<p><small>If you wish to disable this page, create a blank file: /engine/info/disabled.php</small></p>
+		
+		
 	</body>
 </html>
 <?php } else { ?>
