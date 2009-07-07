652ca69ddc9580930f083cac0c302592576145e5
Mubashar Iqbal <Mubs@.(none)>
2009-07-07 00:05:54 -0400

    moved listing mode to the options.yml file

--- config/themes/default/layouts/template.php
+++ config/themes/default/layouts/template.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<?php $listing_mode = $this->filebrowser->get_folder_property('listing_mode', Kohana::config('filebrowser.listing_mode')); ?>
+	<?php 
+  $listing_mode = Kohana::config('filebrowser.listing_mode');
+  $listing_mode = view::get_option('listing_mode', $listing_mode);
+  $listing_mode = $this->filebrowser->get_folder_property('listing_mode', $listing_mode); ?>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	
