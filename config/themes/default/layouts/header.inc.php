b09a53551fd48e04c08a2ab59611d53c242b354b
Arnaud Mercier <arnaud@area17.com>
2009-08-03 14:41:36 +0200

    Let's really collapse the header, not just the logo

--- config/themes/default/layouts/header.inc.php
+++ config/themes/default/layouts/header.inc.php
@@ -89,7 +89,7 @@
     <span class="nav_sep"></span><a href="javascript:void(location.href='http://tinyurl.com/create.php?url='+encodeURIComponent(location.href))">generate tiny url</a>
     <?php } ?>
     <?php if (view::get_option('display_collapse_header', true) and view::get_option('display_header', true)) { ?>
-    <span class="nav_sep"></span><a id="showHideSwitch" href="javascript:showHideSwitch('logo', document.getElementById('hideText'));"><FONT id="hideText"><?php print "".$showHideLabel;?></FONT></a>
+    <span class="nav_sep"></span><a id="showHideSwitch" href="javascript:showHideSwitch('header', document.getElementById('hideText'));"><FONT id="hideText"><?php print "".$showHideLabel;?></FONT></a>
     <?php } ?>
 
   </div>
