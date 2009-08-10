e33740801dfaa49a4b08953d4b97088fddb665db
Sebastien Tran <sebastien@area17.com>
2009-08-10 15:33:33 +0200

    Rewrote the showHideSwitch js function and removed the matching inlin js call

--- config/themes/default/js/main.js
+++ config/themes/default/js/main.js
@@ -103,7 +103,7 @@ function setUpClasses() {
 	
 	/* Setting border for 1st child of the breadcrumb */
 	if ($('#navigation')[0]) {
-		$('#navigation').find('span.prev_next').children(':first').addClass("first");
+		$('#navigation').find('span.prev_next').find(":nth-child(1)").addClass("first");
 	}
 	
 	if ($('#content')[0]) {
@@ -115,7 +115,42 @@ function setUpClasses() {
 		$('li:first-child').addClass("first");
 		$('li:last-child').addClass("last");
 	}
+	
+	bindHeaderEvents();
 
+}
+
+/* Show/Hide the header + cookie
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+function bindHeaderEvents() {
+	if ($('#showHideSwitch')[0]) {
+		$('#showHideSwitch').click(function(e) {
+			if ($('#hideText').html() == 'expand header') {
+				$(this).addClass('hideSwitch');
+				$(this).removeClass('showSwitch');
+				$('#hideText').html('collapse header');
+				$('#header').show();
+				createCookie('header', 'showSwitch');
+			} else {
+				$(this).addClass('showSwitch');
+				$(this).removeClass('hideSwitch');
+				$('#hideText').html("expand header");
+				$('#header').hide();
+				createCookie('header', 'hideSwitch');
+			}
+		});
+		
+		/* TODO = handle cookie in IE6
+	}
+}
+
+/* Cookies functions
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+function createCookie(name, value) {
+	var today = new Date();
+	today.setTime(today.getTime()+(24*60*60*1000*365));
+	var expires = "; expires=" + today.toGMTString();
+	document.cookie = name+"=" + value + expires+"; path=/";
 }
 
 /* Gallery Module
@@ -179,51 +214,6 @@ function gallery() {
 
 
 
-/* Browser Detect Lite  v1.01
-   http://www.dithered.com/javascript/browser_detect/index.html
-   modified by Chris Nott (chris@dithered.com)
-–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
-/*function BrowserDetectLite() {
-	var agent = navigator.userAgent.toLowerCase(); 
-	
-	// Browser version
-	this.versionMajor = parseInt(navigator.appVersion); 
-	this.versionMinor = parseFloat(navigator.appVersion); 
-
-	// Browser name
-	this.ns    = (agent.indexOf('mozilla')!=-1 && agent.indexOf('spoofer')==-1 && agent.indexOf('compatible') == -1 && agent.indexOf('opera')==-1 && agent.indexOf('webtv')==-1); 
-	this.ns2   = (this.ns && this.versionMajor == 2); 
-	this.ns3   = (this.ns && this.versionMajor == 3); 
-	this.ns4   = (this.ns && this.versionMajor == 4); 
-	this.ns4up = (this.ns && this.versionMajor >= 4); 
-	this.ns6   = (this.ns && this.versionMajor == 5); 
-	this.ns6up = (this.ns && this.versionMajor >= 5); 
-	this.ie    = (agent.indexOf("msie") != -1); 
-	this.ie3   = (this.ie && this.versionMajor < 4); 
-	this.ie4   = (this.ie && this.versionMajor == 4 && agent.indexOf("msie 4.0") != -1); 
-	this.ie4up = (this.ie && this.versionMajor >= 4); 
-	this.ie5   = (this.ie && this.versionMajor == 4 && agent.indexOf("msie 5.0") != -1); 
-	this.ie55  = (this.ie && this.versionMajor == 4 && agent.indexOf("msie 5.5") != -1);
-	this.ie5up = (this.ie && !this.ie3 && !this.ie4); 
-	this.ie6   = (this.ie && this.versionMajor == 4 && agent.indexOf("msie 6.0") != -1);
-	this.ie6up = (this.ie && !this.ie3 && !this.ie4 && !this.ie5 && !this.ie55); 
-	this.opera = (agent.indexOf("opera") != -1); 
-	this.webtv = (agent.indexOf("webtv") != -1); 
-	this.aol   = (agent.indexOf("aol") != -1); 
-	
-	// Javascript version
-	this.js = 0.0;
-	if (this.ns2 || this.ie3) this.js = 1.0 
-	else if (this.ns3 || this.opera || (document.images && this.ie && !this.ie4up)) this.js = 1.1 
-	else if ((this.ns4 && this.versionMinor <= 4.05) || this.ie4) this.js = 1.2 
-	else if ((this.ns4 && this.versionMinor > 4.05) || this.ie5up) this.js = 1.3 
-	else if (this.ns6up) this.js = 1.4 
-
-	// Platform type
-	this.win   = (agent.indexOf("win")!=-1 || agent.indexOf("16bit")!=-1);
-	this.win32 = (agent.indexOf("win95")!=-1 || agent.indexOf("windows 95")!=-1 || agent.indexOf("win98")!=-1 || agent.indexOf("windows 98")!=-1 || agent.indexOf("winnt")!=-1 || agent.indexOf("windows nt")!=-1 || (this.versionMajor >= 4 && navigator.platform == "win32") || agent.indexOf("win32")!=-1 || agent.indexOf("32bit")!=-1);
-	this.mac   = (agent.indexOf("mac")!=-1);
-}*/
 
 
 /* Remove the focus of the links
@@ -265,34 +255,6 @@ function pop(goPage,nom,windowWidth,windowHeight,style) {
 	if ( !popped.opener) { popped.opener = window; }
 }
 
-
-/* Show/Hide element switch
-–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
-function showHideSwitch (theid, lnk) {
-    if (document.getElementById) {
-        var switch_id = document.getElementById(theid);
-
-        if (switch_id.className == "showSwitch") {
-            // collapse
-            switch_id.className = 'hideSwitch';
-            lnk.innerHTML = "expand header";
-
-            var today = new Date();
-            expires = 365 * 1000 * 60 * 60 * 24;
-            var expires_date = new Date( today.getTime() + (expires) );
-            document.cookie = theid+'=hideSwitch'+'; path=/' + ";expires=" + expires_date.toGMTString();
-        } else { 
-            // expand
-            switch_id.className = 'showSwitch';
-            lnk.innerHTML = "collapse header";
-
-            var today = new Date();
-            expires = 365 * 1000 * 60 * 60 * 24;
-            var expires_date = new Date( today.getTime() + (expires) );
-            document.cookie = theid+'=showSwitch'+'; path=/' + ";expires=" + expires_date.toGMTString();
-        }
-    }
-}
 
 /* InfoHideSwitch
 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
--- config/themes/default/layouts/template.php
+++ config/themes/default/layouts/template.php
@@ -7,21 +7,18 @@
   $listing_mode = $this->filebrowser->get_folder_property('listing_mode', $listing_mode); ?>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	
-  <title><?php print $page_title;?> — <?php print $site_title;?></title>
-
+	<title><?php print $page_title;?> — <?php print $site_title;?></title>
+	<link rel="icon" href="<?php echo view::get_view_url() ?>/favicon.ico" type="image/x-con" />
+	<link rel="shortcut icon" href="<?php echo view::get_view_url() ?>/favicon.ico" type="image/x-con" />
 	<link rel="shortcut icon" type="image/png" href="<?php echo view::get_view_url() ?>/images/logos/favicon.png" />
+	
 	<link href="<?php echo view::get_view_url() ?>/css/main.css" type="text/css" rel="stylesheet" >
 	<link href="<?php echo view::get_view_url()."/css/".$listing_mode ?>.css" type="text/css" rel="stylesheet" >
 	<!--[if IE 7]><link href="<?php echo view::get_view_url() ?>/css/ie7.css" type="text/css" rel="stylesheet" ><![endif]-->
-	<link media="only screen and (max-device-width: 480px)" href="<?php echo view::get_view_url() ?>/css/iphone.css" type="text/css" rel="stylesheet" >
-		 
-	<link rel="icon" href="<?php echo view::get_view_url() ?>/favicon.ico" type="image/x-con" />
-	<link rel="shortcut icon" href="<?php echo view::get_view_url() ?>/favicon.ico" type="image/x-con" />
 	
-	<script language="javascript" type="text/javascript" src="/config/themes/default/js/jquery-1.3.2.min.js"></script>
-	<script language="javascript" type="text/javascript" src="/config/themes/default/js/main.js"></script>
-
+	<!--[if !IE]>-->
+	<link media="only screen and (max-device-width: 480px)" href="<?php echo view::get_view_url() ?>/css/iphone.css" type="text/css" rel="stylesheet" >
+	<!--<![endif]-->
 </head>
 <body>
 	<div id="container">
@@ -46,6 +43,8 @@
 		</div>
 		<?php include("footer.inc.php") ?>
 	</div>
+	<script language="javascript" type="text/javascript" src="/config/themes/default/js/jquery-1.3.2.min.js"></script>
+	<script language="javascript" type="text/javascript" src="/config/themes/default/js/main.js"></script>
 </body>
 </html>
 
