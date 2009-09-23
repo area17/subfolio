7d7314cc93ad388cc6d990b66a89e08c81a7edaf
Arnaud Mercier <arnaud@area17.com>
2009-09-23 16:19:13 +0200

    Reused the download box partial in hideable_download_box

--- config/themes/default/pages/filekinds/_download_box.php
+++ config/themes/default/pages/filekinds/_download_box.php
@@ -25,18 +25,18 @@
 	</a>
 
 	<?php	if (isset($kind)) { ?>
-	<!-- Infos -->
-	<dl>
-		<dt>Kind: </dt><dd><?php echo $kind_display ?></dd>
-		<dt>Last modified: </dt><dd><?php echo format::filedate($file->stats['mtime']) ?></dd>
-		<dt>Size: </dt><dd><?php echo $filesize ?></dd>
-	</dl>
-	<!-- Instructions -->
-  <?php if ($file_kind && isset($file_kind['instructions'])) { ?>
-	<p id='instructions'>Instructions: <?php echo $file_kind['instructions'] ?></p>
+		<!-- Infos -->
+		<dl>
+			<dt>Kind: </dt><dd><?php echo $kind_display ?></dd>
+			<dt>Last modified: </dt><dd><?php echo format::filedate($file->stats['mtime']) ?></dd>
+			<dt>Size: </dt><dd><?php echo $filesize ?></dd>
+		</dl>
+		<!-- Instructions -->
+	  <?php if ($file_kind && isset($file_kind['instructions'])) { ?>
+		<p id='instructions'>Instructions: <?php echo $file_kind['instructions'] ?></p>
+		<?php } ?>
 	<?php } ?>
 	<!-- Download -->
-	<?php } ?>
 	<a href="<?php echo $this->filebrowser->get_file_url(); ?>?download=true" id="download">Download File</a>
 	
 </div>
--- config/themes/default/pages/filekinds/_hideable_download_box.php
+++ config/themes/default/pages/filekinds/_hideable_download_box.php
@@ -1,25 +1,6 @@
 <div id="info">
 	<a href="javascript:InfoHideSwitch('info-box','info-button');" id="info-button" class="">Info</a>
 	<div id="info-box" class="hide">
-		<div id="download_box">
-			<a id="clickable-zone" href="<?php echo $this->filebrowser->get_file_url(); ?>">
-				<!-- Filename / comment -->
-				<p id="filename"><?php echo $this->filebrowser->file ?></p>
-				<p><?php echo format::get_rendered_text($comment) ?></p>
-			</a>
-
-			<!-- Infos -->
-			<dl>
-				<dt>Kind: </dt><dd><?php echo $kind_display ?></dd>
-				<dt>Last modified: </dt><dd><?php echo format::filedate($file->stats['mtime']) ?></dd>
-				<dt>Size: </dt><dd><?php echo $filesize ?></dd>
-			</dl>
-			<!-- Instructions -->
-		  <?php if ($file_kind && isset($file_kind['instructions'])) { ?>
-			<p id='instructions'>Instructions: <?php echo format::get_rendered_text($file_kind['instructions']) ?></p>
-			<?php } ?>
-			<!-- Download -->
-			<a href="<?php echo $this->filebrowser->get_file_url(); ?>?download=true" id="download">Download</a>
-		</div>	
+		<?php include("_download_box.php") ?>
 	</div>
 </div>
