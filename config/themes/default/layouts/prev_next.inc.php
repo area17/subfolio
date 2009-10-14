2e594b28b74cd5a55b0f1061e5c70bdb5415db86
Arnaud Mercier <arnaud@area17.com>
2009-10-14 16:17:26 +0200

    Removed the whole navigation if on root

--- config/themes/default/layouts/prev_next.inc.php
+++ config/themes/default/layouts/prev_next.inc.php
@@ -1,12 +1,11 @@
+<?php if (!SubfolioFiles::is_root()) { ?>
 <div id="navigation">
 	<span class="parent_dir">
 		<?php echo SubfolioFiles::parent_link('Parent Directory'); ?>
   </span>
-
-  <?php if (!SubfolioFiles::is_root()) { ?>
 	<span class="prev_next">		
 		<?php echo SubfolioFiles::previous_link_or_span('Previous', 'Previous Directory', 'previous', 'faded'); ?>
 		<?php echo SubfolioFiles::next_link_or_span('Next', 'Next Directory', 'next', 'faded'); ?>
 	</span>
-  <?php } ?>  
 </div>
+<?php } ?>  
