40337bf86c7d850ed833f69d49f7f67aa6fa377f
Arnaud Mercier <arnaud@area17.com>
2009-10-16 17:56:48 +0200

    Changed language ref to downcase, like the other ones

--- config/themes/default/layouts/prev_next.inc.php
+++ config/themes/default/layouts/prev_next.inc.php
@@ -1,10 +1,10 @@
 <?php if (!SubfolioFiles::is_root()) { ?>
 <div id="navigation">
 	<span class="parent_dir">
-		<?php echo SubfolioFiles::parent_link(SubfolioLanguage::get_text('Parent_Directory')); ?>
+		<?php echo SubfolioFiles::parent_link(SubfolioLanguage::get_text('parent_directory')); ?>
   </span>
 	<span class="prev_next">		
-		<?php echo SubfolioFiles::previous_link_or_span(SubfolioLanguage::get_text('Previous'), SubfolioLanguage::get_text('Previous_Directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?><?php echo SubfolioFiles::next_link_or_span(SubfolioLanguage::get_text('Next'), SubfolioLanguage::get_text('Next_Directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?>
+		<?php echo SubfolioFiles::previous_link_or_span(SubfolioLanguage::get_text('Previous'), SubfolioLanguage::get_text('previous_directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?><?php echo SubfolioFiles::next_link_or_span(SubfolioLanguage::get_text('Next'), SubfolioLanguage::get_text('next_directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?>
 	</span>
 </div>
 <?php } ?>
