699895ed3e32b17841f3991da4a7977a8ed40290
Mubashar Iqbal <Mubs@.(none)>
2009-10-16 09:47:28 -0400

    Ticket #235: More language settings

--- config/themes/default/layouts/prev_next.inc.php
+++ config/themes/default/layouts/prev_next.inc.php
@@ -1,10 +1,10 @@
 <?php if (!SubfolioFiles::is_root()) { ?>
 <div id="navigation">
 	<span class="parent_dir">
-		<?php echo SubfolioFiles::parent_link('Parent Directory'); ?>
+		<?php echo SubfolioFiles::parent_link(SubfolioLanguage::get_text('Parent_Directory')); ?>
   </span>
 	<span class="prev_next">		
-		<?php echo SubfolioFiles::previous_link_or_span('Previous', 'Previous Directory', 'previous', 'faded'); ?><?php echo SubfolioFiles::next_link_or_span('Next', 'Next Directory', 'next', 'faded'); ?>
+		<?php echo SubfolioFiles::previous_link_or_span(SubfolioLanguage::get_text('Previous'), SubfolioLanguage::get_text('Previous_Directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?><?php echo SubfolioFiles::next_link_or_span(SubfolioLanguage::get_text('Next'), SubfolioLanguage::get_text('Next_Directory'), SubfolioLanguage::get_text('previous'), 'faded'); ?>
 	</span>
 </div>
-<?php } ?>  
+<?php } ?>
