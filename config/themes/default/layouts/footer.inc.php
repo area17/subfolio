63eeb8195198db8f2a19b8297ada92fb8fb12b9c
Arnaud Mercier <arnaud@area17.com>
2009-10-20 18:27:45 +0200

    Mofified the CSS so that the user can add his own copyright

--- config/themes/default/layouts/footer.inc.php
+++ config/themes/default/layouts/footer.inc.php
@@ -2,12 +2,14 @@
 
 	<?php echo SubfolioTheme::subfolio_link(); ?> <!-- Users can remove that line -->
 
+	<span class='copyright'>Content ©2009 YourCompanyName — All rights reserved</span>
+
 	<?php if (SubfolioTheme::get_option('display_updated_since')) { ?>
 		<span><?php echo SubfolioLanguage::get_text('updated_since'); ?></span>
 		<?php echo SubfolioFiles::updated_since_link_or_span('lastweek'); ?>
-		<span class="footer_sep">&#8226;</span>
+		<span class="updated_since_sep">&#8226;</span>
 		<?php echo SubfolioFiles::updated_since_link_or_span('lastmonth'); ?>
-		<span class="footer_sep">&#8226;</span>
+		<span class="updated_since_sep">&#8226;</span>
 		<?php echo SubfolioFiles::updated_since_link_or_span('lastvisit'); ?>
 	<?php } ?>
 	
