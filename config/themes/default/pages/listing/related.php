26929ce024b9d1f061eae7f22d5e531e5fa2c77a
Mubashar Iqbal <Mubs@.(none)>
2009-10-21 09:49:36 -0400

    Ticket #332

--- config/themes/default/pages/listing/related.php
+++ config/themes/default/pages/listing/related.php
@@ -1,7 +1,7 @@
 <?php if (SubfolioFiles::have_related()) : ?>
 	
 	<div id="related">
-		<p><?php SubfolioLanguage::get_text('seealso') ?></p>
+		<p><?php echo SubfolioLanguage::get_text('seealso') ?></p>
 		<ul class="<?php echo SubfolioTheme::get_listing_mode() ?>">
 			
 			<?php foreach ( SubfolioFiles::related() as $item) : ?>
