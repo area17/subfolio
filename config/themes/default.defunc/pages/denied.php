ddd99e968010e90144009fbb876df2bd4896f909
Mubashar Iqbal <Mubs@.(none)>
2009-10-23 11:49:43 -0400

    Removing old non API theme

--- config/themes/default.defunc/pages/denied.php
+++
@@ -1,4 +0,0 @@
-<img src='<?php echo view::get_view_url() ?>/images/system/authentification_lock.png' width='59' height='59' border='0' />
-<p><?php echo Kohana::lang('filebrowser.accessdenied'); ?><br/>
-<a href="/login"><?php echo Kohana::lang('filebrowser.loginasadifferentuser');?></a>
-</p>
--- config/themes/default.defunc/pages/filekinds/_hideable_download_box.php
+++
@@ -1,6 +0,0 @@
-<div id="info">
-	<a href="javascript:InfoHideSwitch('info-box','info-button');" id="info-button" class="">Info</a>
-	<div id="info-box" class="hide">
-		<?php include("_download_box.php") ?>
-	</div>
-</div>
