318043b90fe496a886a7aef9e08429b0221f8784
Arnaud Mercier <arnaud@area17.com>
2009-10-26 16:01:33 +0100

    Removed the updated_since links for the iPhone

--- config/themes/default/layouts/footer.inc.php
+++ config/themes/default/layouts/footer.inc.php
@@ -4,7 +4,7 @@
 
 	<!-- <span class='copyright'>Content &copy; 2009 YourCompanyName &ndash; All rights reserved</span> -->
 
-	<?php if (SubfolioTheme::get_option('display_updated_since')) { ?>
+	<?php if (!SubfolioTheme::get_mobile_viewport() && SubfolioTheme::get_option('display_updated_since')) { ?>
 		<span><?php echo SubfolioLanguage::get_text('updated_since'); ?></span>
 		<?php echo SubfolioFiles::updated_since_link_or_span('lastweek'); ?>
 		<span class="updated_since_sep">&#8226;</span>
