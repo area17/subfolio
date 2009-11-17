9fc2a25e0f39cb2461dce4ecd546633a3a6ec7ed
Mubashar Iqbal <mubs@>
2009-11-17 09:04:57 -0500

    Treat all passwords as strings

--- engine/application/libraries/User.php
+++ engine/application/libraries/User.php
@@ -10,8 +10,8 @@ class User {
   public function __construct($name, $array) {
     $this->name     = $name;
         
-    $this->password        = isset($array['password']) ? $array['password'] : '';
-    $this->hashed_password = isset($array['hashed_password']) ? $array['hashed_password'] : '';
+    $this->password        = isset($array['password']) ? "".$array['password'] : '';
+    $this->hashed_password = isset($array['hashed_password']) ? "".$array['hashed_password'] : '';
     $this->admin           = isset($array['admin']) ? $array['admin'] : false;
   }
  
