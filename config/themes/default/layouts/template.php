20e5188eba8d3c9489a791165f815ce1c38317ad
Arnaud Mercier <arnaud@area17.com>
2009-11-20 11:53:10 +0100

    Added the inline overwrite styles for customization

--- config/themes/default/layouts/template.php
+++ config/themes/default/layouts/template.php
@@ -27,6 +27,76 @@
 		<link href="<?php echo SubfolioTheme::get_view_url(); ?>/css/iphone.css" type="text/css" rel="stylesheet" >
 	<?php }	?>
 
+	<!-- All right. Let's overwrite with some inline styles -->
+	<?php
+		// Values from the settings
+		$background_color = 'white';
+		$background_hover_color = '#f5f5f5';
+		$border_color = '#eee';  // If you remove the border, remove the margin-top of #content
+		$line_color = '#eee';
+		$text_strong_color = '#1a1a1a';
+		$text_color = '#333';
+		$text_light_color = '#666';
+		$text_dimmed_color = '#999';
+		$link_color = '#1a1a1a';
+		$flash_color = 'red';
+	?>
+	<style type="text/css" media="screen">
+	
+		/* BACKGROUND */
+		body, #gallery ul li a div.gallery_thumbnail, #lock form input.field
+		{ background-color:<?php echo $background_color ?>; }
+		
+		#lock .login_header b, #gallery ul li a:hover, #gallery ul li a:hover p, #gallery ul li.hover, .subButton input
+		{ color: <?php echo $background_color ?>; }
+		
+		/* BACKGROUND_HOVER */
+		.standard_paragraph code, #breadcrumbtools a:hover, #footer a:hover, #navigation a:hover, #navigation a.hover,
+		#gallery ul li a, #features ul li a:hover, .list li a:hover, .grid li a:hover
+		{ background-color:<?php echo $background_hover_color ?>; }
+		
+		/* LINKS (must stay before the text colors) */ 
+		a, a:link, a:hover, a:active, a:visited, .filename, #navigation a.hover
+		{ color : <?php echo $link_color ?>; }
+		#gallery ul li a:hover, #gallery ul li a:hover p, #gallery ul li.hover, .subButton input:hover
+		{ background: <?php echo $link_color ?>; }
+		
+		/* TEXT */
+		body, #lock .login_header, #footer a, #features ul li a .info p, #features ul li a .info p, .standard_paragraph a,
+		#breadcrumb
+		{ color : <?php echo $text_color ?>; }
+		
+		/* TEXT_STRONG */
+		.standard_paragraph h1, .standard_paragraph h3, .standard_paragraph h4, .standard_paragraph h5,
+		.standard_paragraph strong, .standard_paragraph em, .standard_paragraph b, b, strong
+		{ color: <?php echo $text_strong_color ?>; }
+
+		/* TEXT_LIGHT */
+		.standard_paragraph h2, .standard_paragraph p small, .standard_paragraph p small a, #footer, .listing-header, .list li a
+		{ color: <?php echo $text_light_color ?>; }
+		#notice, .subButton input
+		{ background-color: <?php echo $text_light_color ?>; }
+		
+		/* TEXT_DIMMED */
+		#navigation .prev_next .faded, .grid li span.comment
+		{ color: <?php echo $text_dimmed_color ?>; }
+		
+		/* FLASH */
+		.error { color: <?php echo $flash_color ?>; }
+		#notice.error, #lock .login_feedback { background-color: <?php echo $flash_color ?>; }
+		
+		/* BORDERS < these are the navigation borders */
+		#breadcrumbtools, #navigation, #tools li a, #navigation span.prev_next .first, #navigation span.prev_next *:first-child,
+		#footer, #footer a#footer-home, #footer span.copyright, .subButton
+		{ border-color: <?php echo $border_color ?>; }
+		
+		/* LINES < these are the mandatory lines */
+		.standard_paragraph code, .standard_paragraph blockquote, .standard_paragraph table td, .standard_paragraph table tr:last-child td,
+		#download_box dl, #download_box dd , #download_box #instructions, #info-box, #lock form, #lock form input.field
+		{ border-color: <?php echo $line_color ?>; }
+		
+	</style>
+	
 	<script language="javascript" type="text/javascript" src="<?php echo SubfolioTheme::get_view_url(); ?>/js/common.js"></script>
 	<?php if (SubfolioTheme::get_mobile_viewport()) { ?>
 		<script language="javascript" type="text/javascript" src="<?php echo SubfolioTheme::get_view_url(); ?>/js/main-iphone.js"></script>
