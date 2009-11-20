c073d4d1d7da4a49e1e94c453dbcbecb3d282473
Arnaud Mercier <arnaud@area17.com>
2009-11-20 11:26:40 +0100

    Set the width of the feature text to go with the new feature design

--- config/themes/default/pages/listing/features.php
+++ config/themes/default/pages/listing/features.php
@@ -4,8 +4,8 @@
 	  <ul>
 			<?php foreach ( SubfolioFiles::features() as $feature) : ?>
 		    <li>
-				  <a href="<?php echo $feature['link'] ?>">
-			    	<img src="<?php echo $feature['image_file'] ?>">
+				  <a href="<?php echo $feature['link'] ?>" style="width: <?php echo $feature['image_width'] ?>px">
+			    	<img src="<?php echo $feature['image_file'] ?>" width="<?php echo $feature['image_width'] ?>px" height="<?php echo $feature['image_height'] ?>px">
 			      <div class="info">
 			        <h2><?php echo $feature['title'] ?></h2>
 			        <p><?php echo $feature['description'] ?></p>
