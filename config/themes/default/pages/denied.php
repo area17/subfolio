ba156b42799af75e68146a5a1b3b147efefcc8c2
Arnaud Mercier <arnaud@area17.com>
2009-11-20 11:51:20 +0100

    Replaced the denied:lock icon image by a css element. So we can target IE.

--- config/themes/default/pages/denied.php
+++ config/themes/default/pages/denied.php
@@ -1,4 +1,4 @@
-<img src='<?php echo SubfolioTheme::get_view_url() ?>/images/system/authentification_lock.png' width='59' height='59' border='0' />
+<span class='big_lock'>!</span>
 <p>
 	<?php echo SubfolioLanguage::get_text('accessdenied'); ?><br/>
 	<a href="/login"><?php echo SubfolioLanguage::get_text('loginasadifferentuser');?></a>
