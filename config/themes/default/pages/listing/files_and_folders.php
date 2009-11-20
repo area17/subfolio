112b247015988e6cb63984b8606da3fc20a6a98f
Arnaud Mercier <arnaud@area17.com>
2009-11-20 10:55:04 +0100

    Added the display_column conditions to the file listing

--- config/themes/default/pages/listing/files_and_folders.php
+++ config/themes/default/pages/listing/files_and_folders.php
@@ -10,37 +10,54 @@
 						<img src="<?php echo SubfolioTheme::get_view_url() ?>/images/system/no_icon.png" width='18' height='17' border='0' />
 					</span>
 					<span class="filename"><a href="?sort=filename"><?php echo SubfolioLanguage::get_text('filename'); ?></a></span>
-					<span class="size"><a href="?sort=size"><?php echo SubfolioLanguage::get_text('size'); ?></a></span>
-					<span class="date"><a href="?sort=date"><?php echo SubfolioLanguage::get_text('date'); ?></a></span>
-					<span class="kind"><a href="?sort=kind"><?php echo SubfolioLanguage::get_text('kind'); ?></a></span>
-					<span class="comment"><?php echo SubfolioLanguage::get_text('comment'); ?></span>
+					<?php if (SubfolioTheme::get_option('display_size')) { ?>
+						<span class="size"><a href="?sort=size"><?php echo SubfolioLanguage::get_text('size'); ?></a></span>
+					<?php } ?>
+					<?php if (SubfolioTheme::get_option('display_date')) { ?>
+						<span class="date"><a href="?sort=date"><?php echo SubfolioLanguage::get_text('date'); ?></a></span>
+					<?php } ?>
+					<?php if (SubfolioTheme::get_option('display_kind')) { ?>
+						<span class="kind"><a href="?sort=kind"><?php echo SubfolioLanguage::get_text('kind'); ?></a></span>
+					<?php } ?>
+					<?php if (SubfolioTheme::get_option('display_comment')) { ?>
+						<span class="comment"><?php echo SubfolioLanguage::get_text('comment'); ?></span>
+					<?php } ?>
 				</li>
 			<?php } ?>
 			
 			<?php foreach ( SubfolioFiles::files() as $item) : ?>
 				<li>
 					<a target="<?php echo $item['target'] ?>" href="<?php echo $item['url'] ?>">
-						<?php if ($item['updated']) { ?>
-  			      		<span class="updated"><!-- --></span>
-  					<?php } ?>
-  					<?php if ($item['new']) { ?>
-  			      	<span class="new"><!-- --></span>
-  					<?php } ?>
-  					<?php if ($item['restricted']) { ?>
-  						<span class="<?php if ($item['have_access']) { echo "unlocked"; } else { echo "locked"; } ?>"><!-- --></span>
-  					<?php	} ?>
-						<span class="icon" <?php if (SubfolioTheme::get_mobile_viewport()) { echo "style='background-image:url(".$item['icon_grid'].")'"; } ?>>
-						<?php if (SubfolioTheme::get_listing_mode()=='list') : ?>
-							<img src='<?php echo $item['icon'] ?>' width='18' height='17' />
-						<?php else : ?>
-							<img src='<?php echo $item['icon'] ?>' width='32' height='32' />
+						
+						<!-- ICON -->
+						<?php if (SubfolioTheme::get_option('display_icons')) : ?>
+							<?php if ($item['updated']) { ?>
+	  			      		<span class="updated"><!-- --></span>
+	  					<?php } ?>
+	  					<?php if ($item['new']) { ?>
+	  			      	<span class="new"><!-- --></span>
+	  					<?php } ?>
+	  					<?php if ($item['restricted']) { ?>
+	  						<span class="<?php if ($item['have_access']) { echo "unlocked"; } else { echo "locked"; } ?>"><!-- --></span>
+	  					<?php	} ?>
+							<span class="icon" <?php if (SubfolioTheme::get_mobile_viewport()) { echo "style='background-image:url(".$item['icon_grid'].")'"; } ?>>
+							<?php if (SubfolioTheme::get_listing_mode()=='list') : ?>
+								<img src='<?php echo $item['icon'] ?>' width='18' height='17' />
+							<?php else : ?>
+								<img src='<?php echo $item['icon'] ?>' width='32' height='32' />
+							<?php endif; ?>
+							</span>
 						<?php endif; ?>
-						</span>
+						
+						<!-- FILENAME -->
 						<span class="filename"><?php echo $item['filename'] ?></span>
-						<span class="size"><?php echo $item['size'] ?></span>
-						<span class="date"><?php echo $item['date'] ?></span>
-						<span class="kind"><?php echo $item['kind'] ?></span>
-						<span class="comment"><?php echo $item['comment'] ?></span>
+						
+						<!-- OTHER COLUMNS -->
+						<?php if (SubfolioTheme::get_option('display_size')) { ?><span class="size"><?php echo $item['size'] ?></span><?php } ?>
+						<?php if (SubfolioTheme::get_option('display_date')) { ?><span class="date"><?php echo $item['date'] ?></span><?php } ?>
+						<?php if (SubfolioTheme::get_option('display_kind')) { ?><span class="kind"><?php echo $item['kind'] ?></span><?php } ?>
+						<?php if (SubfolioTheme::get_option('display_comment')) { ?><span class="comment"><?php echo $item['comment'] ?></span><?php } ?>
+							
 					</a>
 				</li>
 			<?php endforeach; ?>
