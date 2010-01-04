59dc42efa4f76d51d4fa1d5a414fc2a719a4993d
AREA 17 <A17@Arnaud-iMac.local>
2010-01-04 19:08:19 +0100

    Text goes over images for all inline modules

--- config/themes/default/pages/listing/inline_middle.php
+++ config/themes/default/pages/listing/inline_middle.php
@@ -1,14 +1,3 @@
-<?php if (SubfolioFiles::have_inline_images('middle')) : ?>
-
-	<div id="inline_middle_image" >
-			<?php foreach ( SubfolioFiles::inline_images('middle') as $image) : ?>
-				<img width='<?php echo $image['width'] ?>' height='<?php echo $image['height'] ?>' src='<?php echo $image['url'] ?>' />
-			<?php endforeach; ?>
-	</div><!-- gallery -->
-
-<?php endif ?>
-
-
 <?php if (SubfolioFiles::have_inline_texts('middle')) : ?>
 
 	<div id="inline_middle_text" class="standard_paragraph">
@@ -19,6 +8,15 @@
 
 <?php endif ?>
 
+<?php if (SubfolioFiles::have_inline_images('middle')) : ?>
+
+	<div id="inline_middle_image" >
+			<?php foreach ( SubfolioFiles::inline_images('middle') as $image) : ?>
+				<img width='<?php echo $image['width'] ?>' height='<?php echo $image['height'] ?>' src='<?php echo $image['url'] ?>' />
+			<?php endforeach; ?>
+	</div><!-- gallery -->
+
+<?php endif ?>
 
 <?php if (SubfolioFiles::have_inline_rss('middle')) : ?>
 
--- config/themes/default/pages/listing/inline_top.php
+++ config/themes/default/pages/listing/inline_top.php
@@ -1,12 +1,3 @@
-<?php if (SubfolioFiles::have_inline_images('top')) : ?>
-	<div id="inline_top_image" >
-			<?php foreach (SubfolioFiles::inline_images('top') as $image) : ?>
-				<img width='<?php echo $image['width'] ?>' height='<?php echo $image['height'] ?>' src='<?php echo $image['url'] ?>' />
-			<?php endforeach; ?>
-	</div><!-- inline top image -->
-<?php endif ?>
-
-
 <?php if (SubfolioFiles::have_inline_texts('top')) : ?>
 
 	<div id="inline_top_text" class="standard_paragraph">
@@ -17,6 +8,13 @@
 
 <?php endif ?>
 
+<?php if (SubfolioFiles::have_inline_images('top')) : ?>
+	<div id="inline_top_image" >
+			<?php foreach (SubfolioFiles::inline_images('top') as $image) : ?>
+				<img width='<?php echo $image['width'] ?>' height='<?php echo $image['height'] ?>' src='<?php echo $image['url'] ?>' />
+			<?php endforeach; ?>
+	</div><!-- inline top image -->
+<?php endif ?>
 
 <?php if (SubfolioFiles::have_inline_rss('top')) : ?>
 
