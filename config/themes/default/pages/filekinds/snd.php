71a150c06ac38ab43d6fce6c84db14ab41b51591
Sebastien Tran <sebastien@area17.com>
2010-02-17 18:07:27 +0100

    Removed duplicated attributes in media filekind

--- config/themes/default/pages/filekinds/snd.php
+++ config/themes/default/pages/filekinds/snd.php
@@ -5,9 +5,8 @@
   pluginspage='http://public.apple.com/quicktime/' 
   width='<?php echo Subfolio::current_file('width') ?>' 
   height='<?php echo Subfolio::current_file('height') ?>' 
-  scale='noscale' 
-  controller='true' 
-  pluginspage='http://public.apple.com/quicktime/'>
+  scale='noscale'>
 </EMBED>
+
 
 <?php if (SubfolioTheme::get_option('display_info')) { require("_hideable_download_box.php"); } ?>
--- config/themes/default/pages/filekinds/vid.php
+++ config/themes/default/pages/filekinds/vid.php
@@ -1,13 +1,11 @@
 <EMBED 
   src='<?php echo Subfolio::current_file('url') ?>' 
   autoplay=<?php echo Subfolio::current_file('autoplay') ?>
-  controller='true' 
   pluginspage='http://public.apple.com/quicktime/' 
   width='<?php echo Subfolio::current_file('width') ?>' 
   height='<?php echo Subfolio::current_file('height') ?>'
   scale='noscale' 
-  controller='true' 
-  pluginspage='http://public.apple.com/quicktime/'>
+  controller='true'>
 </EMBED>
 
 <?php if (SubfolioTheme::get_option('display_info')) { require("_hideable_download_box.php"); } ?>
