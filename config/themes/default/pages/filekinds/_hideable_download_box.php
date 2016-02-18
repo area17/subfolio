6885142c0269b7de4b7848f162aa8a04b2ec75d1
Antoine Doury <antoine@area17.com>
2016-02-18 14:56:34 +0100

    Close icon

--- config/themes/default/pages/filekinds/_hideable_download_box.php
+++ config/themes/default/pages/filekinds/_hideable_download_box.php
@@ -1,5 +1,5 @@
 <div id="info">
-  <a href="#" id="info-button" class="" data-behavior="show_info">i</a>
+  <a href="#" id="info-button" class="icon icon__info" data-behavior="show_info">i</a>
   <div id="info-box" class="hide">
     <?php include("_download_box.php") ?>
   </div>
