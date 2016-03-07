5b3ea0ba540bd6b848d2b8b3df448f30fa333bb3
Antoine <antoine@area17.com>
2016-03-07 21:12:39 +0100

    #14 When logged in with limited access, the 'log in as a different user' link does not work

--- config/themes/default/pages/denied.php
+++ config/themes/default/pages/denied.php
@@ -1,4 +1,8 @@
 <p>
 	<?php echo SubfolioLanguage::get_text('accessdenied'); ?><br/>
+	<?php if (SubfolioUser::is_logged_in()) { ?>
+	<a href="/logout"><?php echo SubfolioLanguage::get_text('loginasadifferentuser');?></a>
+	<?php } else { ?>
 	<a href="/login"><?php echo SubfolioLanguage::get_text('loginasadifferentuser');?></a>
+	<?php } ?>
 </p>
